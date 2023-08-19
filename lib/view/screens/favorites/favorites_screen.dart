60a392a0cfeea45cc2cf61ef1a4f2f44ce123bda
hadeer moustafa <hadeermoustafa118@outlook.com>
2023-08-19 22:32:58 +0300

    favourite issue solved

--- lib/view/screens/favorites/favorites_screen.dart
+++ lib/view/screens/favorites/favorites_screen.dart
@@ -1,63 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gallery/model/models/base_models/photos.dart';
 import 'package:gallery/view_model/cubit/favourites_cubit/favourites_cubit.dart';
-import 'package:gallery/view_model/cubit/home_cubit/home_cubit.dart';
 import '../../widgets/photo_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-
+  const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    FavouritesCubit cubit = FavouritesCubit();
     return BlocProvider(
-      create: (context) => FavouritesCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-        ),
-        body: BlocBuilder<HomeCubit, HomeState>(
+        create: (context) => FavouritesCubit()..favourites(),
+        child: BlocConsumer<FavouritesCubit, FavouritesState>(
+          listener: (context, state) {
+            if (state is FavouritesCubit) {
+              print('FavouritesCubit');
+            } else {
+              print('error');
+            }
+          },
           builder: (context, state) {
-            return BlocConsumer<HomeCubit, HomeState>(
-              listener: (context, state) {
-
-              },
-              builder: (context, state) {
-                return BlocConsumer<FavouritesCubit, FavouritesState>(
-                  listener: (context, state) {
-                    if(state is FavouritesCubit){
-                      print('FavouritesCubit');
-                    }else {
-                      print('error');
-                    }
-                  },
-                  builder: (context, state) {
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      itemCount: cubit.photos.length,
-                      padding: const EdgeInsets.all(15),
-                      itemBuilder: (context, index) =>
-                          PhotoItem(
-                            url: cubit.photos[index],
-                          ),
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: 2 / 3,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                      ),
-                    );
-                  },
-                );
-              },
+            var cubit = FavouritesCubit.get(context);
+            return Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+              ),
+              body: GridView.builder(
+                shrinkWrap: true,
+                itemCount: cubit.photos.length,
+                padding: const EdgeInsets.all(15),
+                itemBuilder: (context, index) => PhotoItem(
+                  url: cubit.photos[index],
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 2 / 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+              ),
             );
           },
-        ),
-      ),
-    );
+        ));
   }
-
 }
-
-
